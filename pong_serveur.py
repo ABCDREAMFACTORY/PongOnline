5f8e8d8aa89dcc0717ce3e5a35d5e2106647f415
ABCDREAMFACTORY <timotheeriviere10@gmail.com>
2025-04-15 20:31:42 +0200

    Update pong_serveur.py

--- pong_serveur.py
+++ pong_serveur.py
@@ -1,6 +1,6 @@
 import pygame
 import time
-
+import random
 timer = time.time()
 
 pygame.init()
@@ -9,39 +9,41 @@ fenetreHauteur = 720  #  1000 #720
 screen = pygame.display.set_mode((fenetreLargeur, fenetreHauteur))
 clock = pygame.time.Clock()
 running = True
+font = pygame.font.Font("assets/NotoSans-Bold.ttf", 30)
 
 class Square:
     def __init__(self):
         self.player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-        self.axis_x,self.axis_y = 1,0
-        self.vitesse = 1
-
+        self.axis_x,self.axis_y = 1,random.randint(-1,1)
+        self.vitesse = 700
     def draw(self):
         pygame.draw.circle(screen, "white", self.player_pos, 5)
-        
-    def move(self):
+
+    def move(self,time):
         self.collidej1 = self.collision(j1)
         self.collidej2 = self.collision(j2)
         if self.collidej2: 
             self.axis_x = -1
-
+            self.axis_y = random.randint(-1,1)
         if self.collidej1: 
             self.axis_x = 1
-
+            self.axis_y = random.randint(-1,1)
         if self.player_pos.x >= fenetreLargeur:
             j1.score += 1
-
+            afficher = font.render(f"score: {j1.score}/{j2.score}", 1,(255,255,255))
+            restart(afficher)
         elif self.player_pos.x <= 0:
             j2.score += 1
-
+            afficher = font.render(f"score: {j1.score}/{j2.score}", 1,(255,255,255))
+            restart(afficher)
         if self.player_pos.y <= 0:
             self.axis_y = 1
 
-        elif self.player_pos.y >= fenetreHauteur:  
+        elif self.player_pos.y >= fenetreHauteur-10:  
             self.axis_y = -1
 
-        self.player_pos.x += self.axis_x*self.vitesse
-        self.player_pos.y += self.axis_y*self.vitesse
+        self.player_pos.x += self.axis_x*self.vitesse*time
+        self.player_pos.y += self.axis_y*self.vitesse*time
 
     def collision(self,player):
          # Trouve le point le plus proche du centre du cercle sur le rectangle
@@ -57,12 +59,28 @@ class Square:
 
 class Player:
     def __init__(self,x):
-        self.rect = (fenetreLargeur*x, fenetreHauteur//2-fenetreLargeur//100, fenetreLargeur//200, fenetreHauteur//25)
+        self.rect = [fenetreLargeur*x, fenetreHauteur/2-fenetreLargeur/100, fenetreLargeur/200, fenetreHauteur/15]
         self.score = 0
+        self.vitesse = 500
     def draw(self):
         pygame.draw.rect(screen,"white",self.rect)
-
-
+    def move_up(self,time):
+        if self.rect[1] > 0:
+            self.rect[1] -= self.vitesse*time
+    def move_down(self,time):
+        if self.rect[1] < fenetreHauteur-self.rect[3]:
+            self.rect[1] += self.vitesse*time
+def restart(afficher):
+    
+    j1.rect = [fenetreLargeur*0.1, fenetreHauteur/2-fenetreLargeur/100, fenetreLargeur/200, fenetreHauteur/15]
+    j2.rect = [fenetreLargeur*0.9, fenetreHauteur/2-fenetreLargeur/100, fenetreLargeur/200, fenetreHauteur/15]
+    s.__init__()
+    j1.draw()
+    j2.draw()
+    s.draw()
+    screen.blit(afficher, (20,20))
+    pygame.display.flip()
+    time.sleep(1)
 s = Square()
 j1 = Player(0.1)
 j2 = Player(0.9)
@@ -73,11 +91,22 @@ while running:
         if event.type == pygame.QUIT:
             running = False
     screen.fill("black")
+    delta_time = clock.tick(60) / 1000.0
     
-    s.draw()
-    if time.time() - timer > 0.1:
-        s.move()
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_z]:
+        j1.move_up(delta_time)
+    if keys[pygame.K_s]:
+        j1.move_down(delta_time)
+    if keys[pygame.K_UP]:
+        j2.move_up(delta_time)
+    if keys[pygame.K_DOWN]:
+        j2.move_down(delta_time)
+
         #timer = time.time()
+    s.draw()
+    s.move(delta_time)
     j1.draw()
     j2.draw()
     pygame.display.flip()
+    clock.tick(60)
